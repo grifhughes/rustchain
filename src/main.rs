0784334eeeb7564ebb73a179126580d72a136f81
Grif Hughes <grifhughes2@gmail.com>
2016-02-18 13:42:49 -0600

    Added kill process test

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
+#![feature (libc)]
+extern crate libc;
 extern crate hyper;
+
 pub mod wallet;
 mod conversions;
 mod command_line;
 
 use std::io;
+use std::thread;
 use std::io::Read;
 use hyper::Client;
 use hyper::header::Connection;
-use std::thread;
 use std::time::Duration;
+use libc::{kill, SIGTERM};
 
 fn main() {
-    command_line::initialize_server().unwrap_or_else(|e| { panic!("Failed to start server"); });
+    let child = command_line::initialize_server().unwrap_or_else(|e| { panic!("Failed to start server: {}", e); });
     
     let mut user_wallet: wallet::Wallet = Default::default();
     
     thread::sleep(Duration::from_millis(1200));
+    
     println!("Enter wallet identifier: ");  
     io::stdin().read_line(&mut user_wallet.guid).expect("Failed to read");
     
@@ -29,21 +34,16 @@ fn main() {
     println!("Enter amount: (BTC)");
     let mut amount_satoshi = String::new();
     io::stdin().read_line(&mut amount_satoshi).expect("Failed to read");
-    let amount_btc = conversions::satoshi_to_btc(amount_satoshi.parse::<f32>().expect("Error: failed to parse")).to_string();
+    let amount_btc = conversions::satoshi_to_btc(amount_satoshi.trim().parse::<f32>().expect("Error: failed to parse")).to_string();
    
     let f1 = "http://localhost:3000/merchant/".to_string();
-    let f5 = "http://localhost:3000/merchant/".to_string();
     let f2 = "/payment?password=";
     let f3 = "&to=";
     let f4 = "&amount=";
     let url = f1 + &user_wallet.guid + f2 + &user_wallet.main_password + f3 + &dest_addr + f4 + &amount_btc;
-    let login_url = f5 + &user_wallet.guid + "/login?password=" + &user_wallet.main_password + "&api_code=";
+    println!("{}", url);
 
     let client = Client::new();
-    
-    let mut res = client.get(&login_url).header(Connection::close()).send().unwrap();
-    let mut body = String::new();
-    res.read_to_string(&mut body).unwrap();
     
     let mut res = client.get(&url).header(Connection::close()).send().unwrap();
     let mut body = String::new();
@@ -51,5 +51,7 @@ fn main() {
 
     println!("Response: {}", body);
 
-    
+    unsafe {
+        kill(child.id() as i32, SIGTERM);
+    }    
 }
