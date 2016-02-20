eb8d8651ca030abff5ad6d6209382fa079e1758c
Grif Hughes <ghughes@smu.edu>
2016-02-19 19:06:28 -0600

    Added WORST login check in the history of login checks

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature (libc)]
+#![feature (libc, convert)]
 extern crate libc;
 extern crate hyper;
 
@@ -8,18 +8,19 @@ mod command_line;
 
 use std::io;
 use std::thread;
+
 use std::io::Read;
 use hyper::Client;
 use hyper::header::Connection;
 use std::time::Duration;
 use libc::{kill, SIGTERM};
 
-fn query_url(url: String, cl: &Client) {
+fn query_url(url: String, cl: &Client) -> String {
     let mut res = cl.get(&url).header(Connection::close()).send().unwrap();
     let mut json_response = String::new();   
     res.read_to_string(&mut json_response).unwrap();
-   
-    println!("Response: {}", json_response);
+
+    json_response
 }
 
 fn main() {
@@ -33,41 +34,48 @@ fn main() {
     thread::sleep(Duration::from_millis(1200));
 
     println!("Login to wallet\nEnter wallet identifier:");
-    input.read_line(&mut user_wallet.guid).expect("Failed to read");
+    let mut guid = String::new();
+    input.read_line(&mut guid).expect("Failed to read");
+    user_wallet.guid = guid.trim().to_string();
     
     println!("Enter wallet password: ");
-    input.read_line(&mut user_wallet.main_password).expect("Failed to read");
+    let mut password = String::new();
+    input.read_line(&mut password).expect("Failed to read");
+    user_wallet.main_password = password.trim().to_string();
        
-    query_url(user_wallet.login(), &client);
+    if query_url(user_wallet.login(), &client).as_str().contains("true") {
+        println!("Successful login...");
 
-    //assumes successful login, TODO add error checking
-    println!("1 - send payment\n2 - fetch wallet balance\n3 - fetch balance at specific address\n4 - list wallet addresses");
+        println!("1 - send payment\n2 - fetch wallet balance\n3 - fetch balance at specific address\n4 - list wallet addresses");
 
-    let mut option = String::new();
-    input.read_line(&mut option).expect("Failed to read");
+        let mut option = String::new();
+        input.read_line(&mut option).expect("Failed to read");
+        
+        match option.trim().parse::<i8>().unwrap() {
+            1 => {
+                println!("Enter destination address:");
+                let mut destination = String::new();
+                input.read_line(&mut destination).expect("Failed to read");
+                
+                println!("Enter amount in BTC:");
+                let mut amount_btc = String::new();
+                input.read_line(&mut amount_btc).expect("Failed to read");
+                
+                query_url(user_wallet.send_payment(&destination.trim(), conversions::btc_to_satoshi(amount_btc.trim().parse::<f32>().expect("Failed to parse"))), &client);
+            },
+            2 => println!("{}", query_url(user_wallet.wallet_balance(), &client)),
+            3 => {
+                println!("Enter specific addresss:");
+                let mut address = String::new();
+                input.read_line(&mut address).expect("Failed to read");
 
-    match option.trim().parse::<i8>().unwrap() {
-        1 => {
-            println!("Enter destination address:");
-            let mut destination = String::new();
-            input.read_line(&mut destination).expect("Failed to read");
-            
-            println!("Enter amount in BTC:");
-            let mut amount_btc = String::new();
-            input.read_line(&mut amount_btc).expect("Failed to read");
-            
-            query_url(user_wallet.send_payment(&destination, conversions::btc_to_satoshi(amount_btc.trim().parse::<f32>().expect("Failed to parse"))), &client);
-        },
-        2 => query_url(user_wallet.wallet_balance(), &client),
-        3 => {
-            println!("Enter specific addresss:");
-            let mut address = String::new();
-            input.read_line(&mut address).expect("Failed to read");
-
-            query_url(user_wallet.address_balance(&address), &client);
-        },
-        4 => query_url(user_wallet.address_list(), &client),
-        _ => panic!("Error invalid option")
+                query_url(user_wallet.address_balance(&address.trim()), &client);
+            },
+            4 => println!("{}", query_url(user_wallet.address_list(), &client)),
+            _ => panic!("Error invalid option")
+        }
+    } else {
+        println!("Invalid login, exiting...");
     }
     
     unsafe {
