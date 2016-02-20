a0f946087fec05339ea5b7da9f7a902bd9500cf2
Grif Hughes <ghughes@smu.edu>
2016-02-20 00:14:47 -0600

    Fixed address generation

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,7 @@ pub mod wallet;
 mod conversions;
 mod command_line;
 
-use std::io;
-use std::thread;
-use std::fs;
-use std::path;
+use std::{io, thread, fs, path};
 use std::io::Read;
 use hyper::Client;
 use hyper::header::Connection;
@@ -28,7 +25,7 @@ fn query_url(url: String, cl: &Client) -> String {
 }
 
 fn main() {
-    let child = command_line::initialize_server().unwrap_or_else(|e| { panic!("Failed to start server: {}", e); });
+    let mut child = command_line::initialize_server().unwrap_or_else(|e| { panic!("Failed to start server: {}", e); });
 
     let input = io::stdin();
     
@@ -43,7 +40,7 @@ fn main() {
         if query_url(wallet.login(), &client).as_str().contains("true") {
             println!("Successful login...");
 
-            println!("1 - send payment\n2 - fetch wallet balance\n3 - fetch balance at specific address\n4 - list wallet addresses");
+            println!("1 - send payment\n2 - fetch wallet balance\n3 - fetch balance at specific address\n4 - list wallet addresses\n5 - generate new address\n6 - generate new address with specified label");
 
             let mut option = String::new();
             input.read_line(&mut option).expect("Failed to read");
@@ -69,6 +66,19 @@ fn main() {
                     query_url(wallet.address_balance(&address.trim()), &client);
                 },
                 4 => println!("{}", query_url(wallet.address_list(), &client)),
+                5 => {
+                    println!("Generating new address...");
+                    println!("{}", query_url(wallet.generate_address(), &client));
+                    thread::park_timeout(Duration::from_millis(2000));
+                },
+                6 => {
+                    println!("Enter desired label:");
+                    let mut label = String::new();
+                    input.read_line(&mut label).expect("Failed to read");
+                    println!("Generating new address with label {}...", label);
+                    query_url(wallet.generate_address_with_label(&label), &client);
+                    thread::park_timeout(Duration::from_millis(2000));
+            },
                 _ => panic!("Error invalid option")
             }
         } else {
@@ -77,20 +87,20 @@ fn main() {
     } else {
         println!("Performing first time setup...");
 
-        let mut new_wallet: wallet::Wallet = Default::default();
+        let mut first_wallet: wallet::Wallet = Default::default();
 
         println!("Enter blockchain wallet identifier:");
         let mut guid = String::new();
         input.read_line(&mut guid).expect("Failed to read");
-        new_wallet.guid = guid.trim().to_string();
+        first_wallet.guid = guid.trim().to_string();
         
         println!("Enter blockchain wallet password: ");
         let mut password = String::new();
         input.read_line(&mut password).expect("Failed to read");
-        new_wallet.main_password = password.trim().to_string();
+        first_wallet.main_password = password.trim().to_string();
 
         let mut writer = io::BufWriter::new(fs::File::create("wallet.bin").unwrap());
-        bincode::serde::serialize_into(&mut writer, &new_wallet, bincode::SizeLimit::Infinite).unwrap();
+        bincode::serde::serialize_into(&mut writer, &first_wallet, bincode::SizeLimit::Infinite).unwrap();
         println!("Initial wallet configured, exiting...");
     }
     
--- src/wallet.rs
+++ src/wallet.rs
@@ -2,6 +2,7 @@ use conversions;
 
 const API_CODE: &'static str = "581dfe1f-34fc-4660-abe7-c2d0f104a546";
 const MERCHANT_ENDPOINT: &'static str = "http://localhost:3000/merchant/";
+const NEW_WALLET_ENDPOINT: &'static str = "http://localhost:3000/v2/create";
 
 #[derive(Serialize, Deserialize, Default)]
 pub struct Wallet {
@@ -17,6 +18,11 @@ impl Wallet {
         MERCHANT_ENDPOINT.to_string() + &self.guid + "/login?password=" + &self.main_password + "&api_code=" + API_CODE
     }
 
+    //TODO: Implement in main
+    pub fn create_new(&self) -> String {
+        NEW_WALLET_ENDPOINT.to_string() + "?password=" + &self.main_password + "&api_code=" + API_CODE
+    }
+    
     pub fn send_payment(&self, dest_addr: &str, amount_btc: f32) -> String {
         let amount_satoshi_string = conversions::btc_to_satoshi(amount_btc).to_string();
         MERCHANT_ENDPOINT.to_string() + &self.guid + "/payment?password=" + &self.main_password + "&to=" + dest_addr + "&amount=" + &amount_satoshi_string
@@ -42,5 +48,13 @@ impl Wallet {
 
     pub fn address_balance(&self, addr: &str) -> String {
         MERCHANT_ENDPOINT.to_string() + &self.guid + "/address_balance?address=" + addr + "&password=" + &self.main_password
-    } 
+    }
+
+    pub fn generate_address(&self) -> String {
+        MERCHANT_ENDPOINT.to_string() + &self.guid + "/new_address?password=" + &self.main_password
+    }
+
+    pub fn generate_address_with_label(&self, label: &str) -> String {
+        MERCHANT_ENDPOINT.to_string() + &self.guid + "/new_address?password=" + &self.main_password + "&label=" + label
+    }  
 }
